f4a49765d4ab18749a1304e1fa0e902e172ae02b
John-Kimani <kimwaweru100@gmail.com>
2022-04-03 00:16:23 +0300

    Creats my blog templates

--- myblog/views.py
+++ myblog/views.py
@@ -5,5 +5,6 @@ from . models import Post
 
 def post(request):
     # message ='We are a having a test session in Django'
-    return HttpResponse('We are a having a test session in Django')
+    # return HttpResponse('We are a having a test session in Django')
     # return render(request, 'myblog/index.html', {'message':message})
+    return render(request, 'index.html',{})
