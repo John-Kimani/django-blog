f0ce8df7ffa55e4177997ced1f4774d685b68c6c
John-Kimani <kimwaweru100@gmail.com>
2022-04-03 00:50:36 +0300

    Login logic

--- members/views.py
+++ members/views.py
@@ -5,8 +5,21 @@ from django.contrib import messages
 
 
 def login_user(request):
-
-    return render(request,'login.html', {})
+    if request.method == 'POST':
+        username = request.POST['username'] 
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('post')
+        else:
+            # Return an 'invalid login' error message.
+            messages.success(request, ('There was an error login in try again'))
+            return redirect('login')
+    
+    else:
+        return render(request,'login.html', {})
 
 
 
